1eaf124113537190337c36e62c1131e14b589748
Nina Low <z.low@utoronto.ca>
2026-01-27 11:36:53 -0500

    Update and rename episode_cob_traditional.py to define_episode_cob_traditional.py
    added medadv == 1

--- define_episode_cob_traditional.py
+++ define_episode_cob_traditional.py
@@ -153,6 +153,7 @@ def process_patient_chunk(args):
               AND SVCDATE IS NOT NULL 
               AND DATATYP = 3
               AND COB IS NOT NULL
+              AND MEDADV !=1
         """).df()
         
         conn.close()
